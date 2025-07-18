a0d2da82d0887aa2fecd08ca5bd5f02ecd0110a0
BBKML <bookeleblan@gmail.com>
2025-07-18 21:17:21 +0000

    Ajout de la création automatique du superadmin dans apps.py

--- Suiv/apps.py
+++ Suiv/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+import sys
 
 
 class SuivConfig(AppConfig):
@@ -6,8 +7,18 @@ class SuivConfig(AppConfig):
     name = 'Suiv'
 
     def ready(self):
+        # Import du fichier personnalisé
         import Suiv.signals
 
-      # Import du fichier personnalisé
+        # Création automatique du superadmin si aucun n'existe
+        if any(cmd in sys.argv for cmd in ['runserver', 'migrate', 'createsuperuser', 'shell']):
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+            if not User.objects.filter(is_superuser=True).exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password="adminpassword"
+                )
 
 
